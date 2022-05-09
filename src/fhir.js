8e86b64bc05c3c7c077eb9e52ccc4e196276cfe8
natjoe4 <njjones@mitre.org>
2022-05-09 16:43:47 -0400

    added unit testing, correcting bugs

--- src/fhir.js
+++ src/fhir.js
@@ -1,5 +1,6 @@
 const { FHIRObject, load } = require('cql-exec-fhir');
 const FHIRv401XML = require('../modelInfos/fhir-modelinfo-4.0.1.xml.js');
+const patientReferences = require('./patient-references.json');
 
 /**
  * Iterator for the patients provided to the execution engine
@@ -77,10 +78,57 @@ class Patient extends FHIRObject {
    * @param {Object} retrieveDetails Structure of the information about an ELM retrieve
    */
   // eslint-disable-next-line no-unused-vars
-  findRecords(profile, retrieveDetails) {}
+  async findRecords(profile, retrieveDetails) {
+    const classInfo = getClassInfo(profile, retrieveDetails, this._modelInfo);
+    if (classInfo == null) {
+      console.error(`Failed to find type info for ${profile}`);
+      return [];
+    }
 
-  // add current pat, next pat, for patient source, find records for patient class, load patient ids to pop array w strings (same as asyncpatsource)
-  // https://github.com/cqframework/cql-execution/blob/master/src/types/cql-patient.interfaces.ts
+    const resourceType = classInfo.name.replace(/^FHIR\./, '');
+    // If the patient resource type is requested, return array with just this resource
+    if (resourceType === 'Patient') {
+      return [this];
+    }
+
+    const refKeys = patientReferences[resourceType];
+    const db = this._mongoConnection.db().collection(resourceType);
+    let records = [];
+    // If the resource cannot reference Patient, return all resources in the collection
+    if (!refKeys) {
+      records = await db.find({}, { projection: { _id: 0 } }).toArray();
+    } else {
+      const allQueries = [];
+      records = refKeys.map(async searchTerm => {
+        const query = {};
+        query[`${searchTerm}.reference`] = `Patient/${this._json.id}`;
+        allQueries.push(query);
+      });
+      records = await db.find({ $or: allQueries }, { projection: { _id: 0 } }).toArray();
+    }
+    return records.map(r => new FHIRObject(r, classInfo, this._modelInfo));
+  }
+}
+
+// add current pat, next pat, for patient source, find records for patient class, load patient ids to pop array w strings (same as asyncpatsource)
+// https://github.com/cqframework/cql-execution/blob/master/src/types/cql-patient.interfaces.ts
+
+/**
+ * Retrieves the class information from the modelInfo
+ * @param {String} profile link to a FHIR profile
+ * @param {Object} retrieveDetails structure of the information about an ELM resource
+ * @param {Object} _modelInfo The modelInfo including information on the passed-in profile
+ * @returns {Object} the class information as stored in the modelInfo
+ */
+function getClassInfo(profile, retrieveDetails, _modelInfo) {
+  let classInfo = null;
+  if (retrieveDetails) {
+    classInfo = _modelInfo.findClass(retrieveDetails.datatype);
+  } else {
+    classInfo = _modelInfo.findClass(profile);
+  }
+
+  return classInfo;
 }
 
 module.exports = {
--- test/fhir.test.js
+++ test/fhir.test.js
@@ -1,8 +1,12 @@
 const { PatientSource } = require('../src/fhir');
+const { testSetup, connection, testCleanup } = require('./setup');
+const { FHIRObject } = require('cql-exec-fhir');
 const testPatients = require('./fixtures/testPatients.json');
-const { testSetup, connection } = require('./setup');
+const testProcedures = require('./fixtures/testProcedures.json');
+const testLibraries = require('./fixtures/testLibrary.json');
 
 const TEST_PATIENT_IDS = testPatients.map(e => e.id);
+const ALL_TEST_RESOURCES = [...testProcedures, ...testPatients, ...testLibraries];
 describe('Patient Source', () => {
   beforeAll(async () => await testSetup(JSON.parse(JSON.stringify(testPatients))));
 
@@ -38,5 +42,41 @@ describe('Patient Source', () => {
     expect(ps._index).toEqual(0);
   });
 
-  afterAll(async () => await connection.close());
+  afterAll(testCleanup);
+});
+
+describe('Patient', () => {
+  beforeAll(async () => await testSetup(JSON.parse(JSON.stringify(ALL_TEST_RESOURCES))));
+  test('findRecords on Procedure returns all valid procedure resources', async () => {
+    const ps = PatientSource.FHIRv401(connection);
+    ps.loadPatientIds(TEST_PATIENT_IDS);
+    const patient = await ps.currentPatient();
+    const records = await patient.findRecords('Procedure');
+
+    expect(records).toHaveLength(2);
+    expect(records[0]).toBeInstanceOf(FHIRObject);
+    expect(records[1]).toBeInstanceOf(FHIRObject);
+
+    const recordsJson = records.map(r => r._json);
+    expect(recordsJson).toEqual(expect.arrayContaining([testProcedures[0], testProcedures[1]]));
+  });
+
+  test('findRecords on Procedure correctly returns no resources', async () => {
+    const ps = PatientSource.FHIRv401(connection);
+    ps.loadPatientIds(TEST_PATIENT_IDS);
+    const patient = await ps.nextPatient();
+    const records = await patient.findRecords('Procedure');
+
+    expect(records).toHaveLength(0);
+  });
+
+  test('findRecords on Library correctly returns all Libraries', async () => {
+    const ps = PatientSource.FHIRv401(connection);
+    ps.loadPatientIds(TEST_PATIENT_IDS);
+    const patient = await ps.currentPatient();
+    const records = await patient.findRecords('Library');
+
+    expect(records).toHaveLength(1);
+  });
+  afterAll(testCleanup);
 });
